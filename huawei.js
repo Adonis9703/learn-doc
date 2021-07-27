14061c6d9cdbf565fd4877511999177293cbcfce
YUSIYUAN <1164818708@qq.com>
2021-07-27 21:14:58 +0800

    UPDATE

--- huawei.js
+++ huawei.js
@@ -2,23 +2,28 @@ const handler = (s, p) => {
   if (s.includes(p)) {
     return 0
   }
+  let idxP = 0
+  let idxS = s.indexOf(p[0])
+  let start = s.indexOf(p[0])
   let result = []
-  //avaaxbyzab
-  //ab
-  let idxs = 0
-  let idxp = 0
-  let start = []
-  let end = 0
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === p[0]) {
-      start.push(i)
+  while (idxS < s.length) {
+    if (s[idxS] === p[idxP] && s[idxS] !== p[0]) { //如果字符相等且字符不是p[0]
+      if (idxP === p.length - 1) { //如果遍历完一次P，说明匹配完全
+        idxP = 0 //将p指针归零，从头再次匹配
+        result.push(idxS-start-p.length+1) //将答案保存
+      } else { //如果p没有遍历完，p和s同时向后移
+        idxP++
+        idxS++
+      }
+    } else if (s[idxS] === p[0]){ //如果字符匹配到p[0]，重置start标记
+      start = idxS
+      idxP++
+      idxS++
+    } else { //如果不相等 则将s向后移动
+      idxS++
     }
   }
-
-
-
-  //s xxxxxxxxxaxbcabc
-  //p abc
-  //
+  return result
 }
+console.log(handler('axxxbcxxaxbcxb', 'abc'))
+console.log(handler('axb', 'ab'))
